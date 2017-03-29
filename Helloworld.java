77df7a99e55e58a72d54e19fb7c7eb89092a5f47
luiserq12 <luis.ostos@softtek.com>
2017-03-29 16:59:27 -0600

    Segundo Commit, Commit from GitHub

--- Helloworld.java
+++ Helloworld.java
@@ -3,7 +3,7 @@ package com.git.examen;
 public class Helloworld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello GitHub!");
     }
 
 }
